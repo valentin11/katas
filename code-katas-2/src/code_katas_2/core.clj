befaa4db69183889797b6c6b49ab743f8f03c765
Valentin Fernández <valentinfer11@gmail.com>
2014-09-02 01:47:01 -0300

    Third Commit
    Tercer commit con la funcion "unpartial" implementada,la cual pasa los
    tests.
    Falsa terminar la funcion "tartamudeo".

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -5,28 +5,29 @@
   "Escribir una funcion que acepte una funcion parcial con cantidad de argumentos desconocida,
    retornar una funcion equivalente de n argumentos"
   [f]
-  ;(fn [& arg] (apply f arg))
-  )
+  (fn [& seqs] 
+    (loop [f f seqs seqs]
+      (if(not (fn? f))
+        f
+        (recur (f (first seqs)) (rest seqs)))))
+)
 
 
 (defn search
   "Dado un numero cualquiera de secuencias, cada una ya ordenada de menor a mayor, encontrar el numero
    mas chico que aparezca en todas las secuencias, las secuencias pueden ser infinitas."
   [& seqs]
-  (def function (fn [v e] (not-every? false?(map #(= % e) v))));Devuelve true si el vector "v" contiene el elemento "e", sino devuelve false. 
-  (defn intersection [e];Devuelve true si el elemento pertenece a todos los vectores los cuales estan adentro de seqs.
-     (loop [number e vect seqs l []]
+(def function (fn [v e] (not-every? false?(map #(= % e) v))));Devuelve true si el vector "v" contiene el elemento "e", sino devuelve false. 
+(defn intersection [e];Devuelve true si el elemento pertenece a todos los vectores los cuales estan adentro de seqs.
+   (loop [number e vect seqs l []]
        (if (empty? vect)
          (not-any? false? l)
          (recur number (rest vect) (conj l (function (first vect) number))))))
   
-   (if (= (count seqs) 1)
+ (if (= (count seqs) 1)
      (first (first seqs));Devuelve el primer elemento.
-     (first (filter intersection (first seqs)))
+   (first (filter intersection (first seqs)))
    )
-   
-   ;(map (fn [v](set v)) seqs);Paso los vectores a set.
-  ;(first(lazy-seq (clojure.set/intersection (map (fn [v](lazy-seq (set v))) seqs))));Pasarle los elementos dentro de una lista, NO la lista!
 )
 
 
@@ -35,18 +36,18 @@
    retorne una nueva coleccion donde el valor es insertado intercalado cada dos argumentos
    que cumplan el predicado"
   [predicado valor secuencia]
-  (if (empty? secuencia) 
-    secuencia
-    (loop [p predicado v valor s secuencia l []]
-      (if  (= (count s) 1);Caso base.
-         (conj l (first s))
-         (recur p v (rest s) (if (p (first s) (second s))
-                         (conj (conj l (first s)) valor)
-                         (conj l (first s)))
-                        );Lo pongo acá, porque fue la unica forma que pude hacer que vaya variando el vector l.
-       )
+(if (empty? secuencia) 
+  secuencia
+  (loop [p predicado v valor s secuencia l []]
+    (if  (= (count s) 1);Caso base.
+       (conj l (first s))
+       (recur p v (rest s) (if (p (first s) (second s));Si se cumple el predicado
+                       (conj (conj l (first s)) valor)
+                       (conj l (first s)))
+                      )
      )
    )
+ )
 )
 
 
@@ -57,6 +58,12 @@
    La funcion debe aceptar una secuencia inicial de numeros, y devolver una secuencia infinita de compresiones, donde
    cada nuevo elemento es el elemento anterior comprimido."
   [secuencia]
+  (loop [sec secuencia out-sequence []]
+    (if (empty? out-sequence)
+      (recur secuencia (conj out-sequence (compress secuencia)))
+      (recur secuencia (conj out-sequence (compress (last out-sequence))))
+    )
+  )
   )
 
 
