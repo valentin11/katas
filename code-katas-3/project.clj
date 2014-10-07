3b5948e96d4ab8eff33a1e8a2360ede461bcd28c
Valentin Fernández <valentinfer11@gmail.com>
2014-10-07 20:59:56 -0200

    Second Commit
    Segundo commit con los archivos para poder importarlo desde eclipse.

--- code-katas-3/project.clj
+++ code-katas-3/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.logic "0.8.8"]])
+                 [org.clojure/core.logic "0.8.8"]]
+  :plugins [[lein2-eclipse "2.0.0"]])
